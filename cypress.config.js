6c3376bb581d474f1787bf02bca61f0aca3718e1
egrfly <egrfly@outlook.com>
2024-03-05 03:07:11 +0000

    fix: resolve issues with Cypress environment variables

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require("cypress")
+require('dotenv').config()
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5000',
-    experimentalSessionAndOrigin: true,
     testIsolation: false,
+  },
+  env: {
+    OAUTH_USERNAME: process.env.CYPRESS_OAUTH_USERNAME,
+    OAUTH_PASSWORD: process.env.CYPRESS_OAUTH_PASSWORD,
+    OAUTH_OTP_SECRET: process.env.CYPRESS_OAUTH_OTP_SECRET,
   },
 })

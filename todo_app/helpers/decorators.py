a4ffe4355f351ebcf9f043c987d9bec53beaa72d
egrfly <egrfly@outlook.com>
2024-03-07 22:06:11 +0000

    style: apply functools.wraps decorator inside auth decorators

--- todo_app/helpers/decorators.py
+++ todo_app/helpers/decorators.py
@@ -1,20 +1,22 @@
+from functools import wraps
+
 from flask import redirect
 from flask_dance.contrib.github import github
 
 
 def login_required(original_route):
+    @wraps(original_route)
     def protected_route(*args, **kwargs):
         if not github.authorized:
             return redirect("/login")
         return original_route(*args, **kwargs)
-    protected_route.__name__ = original_route.__name__
     return protected_route
 
 
 def logout_required(original_route):
+    @wraps(original_route)
     def protected_route(*args, **kwargs):
         if github.authorized:
             return redirect("/")
         return original_route(*args, **kwargs)
-    protected_route.__name__ = original_route.__name__
     return protected_route
